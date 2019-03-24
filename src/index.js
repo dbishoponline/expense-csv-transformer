da2592d56eb7a4ce7556d06bc85526907519498b
Dylan Bishop <dbishoponline@gmail.com>
2019-03-23 18:41:07 -0700

    adds error handling if user doesn't specify csv file path

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 require('@babel/polyfill')
 
 // 3rd party modules
+import { isNil } from 'ramda'
 import csv from 'csv'
 import fs from 'fs'
 import { dirname, parse } from 'path'
@@ -13,12 +14,20 @@ import { transform } from './mint-quickbooks'
 const debug = false
 const log = console.log
 const logSuccess = x => log(chalk.green(x))
-newFilePath
-// file paths
-const csvFilePath = process.argv[2].toString()
-const newFilePath = `${dirname(csvFilePath)}/${parse(csvFilePath).name}_transformed_${Date.now()}.csv`
+const logError = x => log(chalk.red(x))
 
-fs.createReadStream(csvFilePath)
+const init = () =>
+  isNil(process.argv[2])
+    ? logError(`Error: did not specify a CSV file. \n\nTry running: \n"mintx path/to/transactions.csv -c"`)
+    : runAction()
+
+const runAction = () => {
+
+  // file paths
+  const csvFilePath = process.argv[2].toString()
+  const newFilePath = `${dirname(csvFilePath)}/${parse(csvFilePath).name}_transformed_${Date.now()}.csv`
+
+  fs.createReadStream(csvFilePath)
   .pipe(
     csv.parse())
   .pipe(
@@ -30,6 +39,8 @@ fs.createReadStream(csvFilePath)
       ? fs.createWriteStream(newFilePath, {flags: 'a'})
       : process.stdout)
 
-logSuccess(`CSV Transformation Complete!`)
-logSuccess(newFilePath)
+  logSuccess(`CSV Transformation Complete!`)
+  logSuccess(newFilePath)
+}
 
+init()
